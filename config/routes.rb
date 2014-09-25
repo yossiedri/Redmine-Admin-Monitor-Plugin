14263bf5671d2086c0c169b79d142c73af154c66
Daniel Felix <d.felix@codeinsider.net>
2014-09-25 11:33:52 +0000

    Improved routing
    This way, it would be still compatible with the current Redmine Release.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
-resources :admin_monitor_alerts
+RedmineApp::Application.routes.draw do
+  resources :admin_monitor_alerts
 
-match 'admin_monitor_alerts/:id/issue', :to => 'admin_monitor_alerts#issue', :via => 'get'
-match 'admin_monitor_alerts/:id/handle', :to => 'admin_monitor_alerts#handle', :via => 'get'
-match 'admin_monitor_alerts/:id/silent', :to => 'admin_monitor_alerts#silent', :via => 'get'
-
+  scope "/admin_monitor_alerts/:id" do
+    get 'issue', :to => 'admin_monitor_alerts#issue'
+    get 'handle', :to => 'admin_monitor_alerts#handle'
+    get 'silent', :to => 'admin_monitor_alerts#silent'
+  end
+end
